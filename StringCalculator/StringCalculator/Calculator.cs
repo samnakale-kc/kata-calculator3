8eb5e300eb542aaeb7cf2f6f159fae96094b5527
samnakale-kc <sam.nakale@kaleidocode.co.za>
2025-03-02 16:33:02 +0200

    Added failing tests for string calculator 1 requirement 1

---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            return 99;
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using StringCalculator;
+
+namespace StringCalculatorTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void GIVEN_EmptyString_WHEN_Adding_THEN_ReturnZero()
+        {
+            // Arrange
+            var calculator = new Calculator();
+            string numbers = "";
+            const int expectedResult = 0;
+
+            // Act
+            int result = calculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void GIVEN_OneOrTwoNumbers_WHEN_Adding_THEN_ReturnSum(string numbers, int expectedResult)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            int result = calculator.Add(numbers);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
